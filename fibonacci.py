96282d0852b313a5deb433b68f3ac2df93a7c7e7
VishnuPrabhas <73926747+VishnuPrabhas@users.noreply.github.com>
2022-07-13 19:25:13 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,7 @@
 # Fibonacci Series
 def fibonacci_seq(number):
-   if number <= 1:
-       return number
-   else:
-       return(fibonacci_seq(number-1) + fibonacci_seq(number-2))
+    if number <= 1:
+        return number
+    else:
+        fib_num = (fibonacci_seq(number-1) + fibonacci_seq(number-2))
+    return fib_num
