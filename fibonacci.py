e1f7634d870ad1758c4d418ddd13f834c8e92ff3
VishnuPrabhas <73926747+VishnuPrabhas@users.noreply.github.com>
2022-07-15 12:17:43 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -4,4 +4,5 @@ def fibonacci_seq(number):
         return number
     else:
         fib_num = (fibonacci_seq(number-1) + fibonacci_seq(number-2))
+    print('Fibonacci Generated')
     return fib_num
